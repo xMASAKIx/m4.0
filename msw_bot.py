cdb71b4fe7b6d88221cf14a748893f19a1e800f3
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-06-02 20:57:21 +0800

    Add new character 'sansan' to msw_bot.py

--- msw_bot.py
+++ msw_bot.py
@@ -21,7 +21,8 @@ PLAYER_MAP = {
     "20372100000100706": {"name": "康娜作者", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/984/1722308667603.png"},
     "20372100000590354": {"name": "照&露西", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/114/1771410723884.png"},
     "20372100005311821": {"name": "照&露西2", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/949/1774672479562.png"},
-    "20372100000684110": {"name": "黑貞", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/49/1721248110286.png"}
+    "20372100000684110": {"name": "黑貞", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/49/1721248110286.png"},
+    "20372100007023711": {"name": "sansan", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/214/1780392703599.png"}
 }
 
 DEFAULT_IMAGE = "https://example.com/default.png"
