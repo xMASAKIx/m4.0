74f0af80fbfc2c74dc87c4d311fba6b765a3fd5d
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-06-14 22:36:32 +0800

    Update msw_bot.py

--- msw_bot.py
+++ msw_bot.py
@@ -26,7 +26,8 @@ PLAYER_MAP = {
     "20372100000737301": {"name": "HEE SABER", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/521/1728129793932.png"},
     "20372100000535168": {"name": "겐지스", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/212/1781100473061.png"},
     "20372100008590559": {"name": "黑豹女", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/726/1781314436642.png"},
-    "20372100005946653": {"name": "詩音", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/556/1748663377570.png"}
+    "20372100005946653": {"name": "詩音", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/556/1748663377570.png"},
+    "20372100000104053": {"name": "沖田2", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/704/1781446719047.png"}
 }
 
 DEFAULT_IMAGE = "https://example.com/default.png"
