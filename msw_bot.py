dcb44374729ab0929da6a37048cf025c441e0625
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-08-14 19:23:53 +0800

    Update msw_bot.py

--- msw_bot.py
+++ msw_bot.py
@@ -32,7 +32,8 @@ PLAYER_MAP = {
     "20372100007184155": {"name": "綠鯊魚", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/685/1782798580537.png"},
     "20372100000528012": {"name": "笨蛋", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/907/1749779226013.png"},
     "20372100001110266": {"name": "步槍王", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/550/1737478667056.png"},
-    "20372100007074222": {"name": "es檔案", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/233/1783667416322.png"}
+    "20372100007074222": {"name": "es檔案", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/233/1783667416322.png"},
+    "20372100003511199": {"name": "檔案頭?", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/256/1768671015251.png"}
 }
 
 
